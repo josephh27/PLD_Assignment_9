327d015cf889825fa88162af417604b8baa5fda4
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2022-02-11 21:44:43 +0800

    Tweaked some designs and added a picture code.

--- PLD_Assignment_9.py
+++ PLD_Assignment_9.py
@@ -1,7 +1,10 @@
 import json
+
 with open("PLD_Assignment_9.json") as f:
     data = json.load(f)
 
+# with open(r"C:\Users\Core i5 HB\OneDrive\Pictures\School Photos\Applicant Photo.jpg") as file:
+    
 # for person in data["Person"]:
 #     print(person["Phone"])
 
@@ -11,7 +14,7 @@ from fpdf import FPDF
 class PDF(FPDF):
     def header(self):
         self.image("pdf_background2.png", 0, 0, 218)
-        self.ln(5)
+        self.ln(0)
 
 # Layout("P", "L")
 # Unit ("mm", "cm", "in")
@@ -35,24 +38,30 @@ pdf.set_font("helvetica", "", 16)
 # h = height
 #
 
+pdf.image("Applicant Photo.jpg", 170, 10, 40)
 count = 0
 for attribute, detail in data["Person"].items():
     count += 1
     detail = str(detail)
 
     if attribute:
-        pdf.set_font("helvetica", "B", 13) 
-        pdf.multi_cell(0, 10, attribute, ln = 1, border = 1, align = "L")       
+        if count == 1 or count == 2:
+            pdf.set_font("helvetica", "B", 13)         
+            pdf.multi_cell(155, 10, attribute, ln = 1, border = "B", align = "L")   
+        else:
+            pdf.set_font("helvetica", "B", 13)         
+            pdf.multi_cell(0, 10, attribute, ln = 1, border = "B", align = "L")      
     if detail:    
         pdf.set_font("helvetica", "", 13)
-        if count == 7:
+        if count == 8:
+            pdf.cell(10, 0)
             detail = detail.replace("'", "").replace("[", "").replace("]", "").replace(",", "").replace("%", "\n")
             print(detail)
-            pdf.multi_cell(0, 10, detail, border = 1, ln = 0, align = "L")
+            pdf.multi_cell(0, 8, detail, border = 0, ln = 0, align = "L")
         else:
-            pdf.multi_cell(0, 10, detail, ln = 1, border = 1, align = "L")   
+            pdf.cell(10, 0)
+            pdf.multi_cell(0, 10, detail, ln = 1, border = 0, align = "L")   
         
 if __name__ == "__main__":
     pdf.output("pdf_1.pdf") 
 
-    
